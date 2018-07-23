218134bb3f44545ea08b97a66d9a17a7500e511d
wkatbey <lunarxparallax@gmail.com>
2018-07-22 23:50:29 -0400

    correcting erros

--- data.php
+++ data.php
@@ -10,7 +10,7 @@
 
   $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
 
-  mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+ // mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
 
   $thankYou="<p>Thank you! Your message has been sent.</p>";
 
