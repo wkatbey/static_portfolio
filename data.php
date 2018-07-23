51c2ab482ace26a31e86e692d5d6fb2c013e0681
wkatbey <lunarxparallax@gmail.com>
2018-07-22 23:46:17 -0400

    Made contact form functional

---
+++ data.php
@@ -0,0 +1,18 @@
+
+<?php
+
+  $data = file_get_contents("php://input")
+  $recipient="lunarxparallax@gmail.com";
+  $subject="Form to email message";
+  $sender=$data["name"];
+  $senderEmail=$data["senderEmail"];
+  $message=$data["message"];
+
+  $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+  mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+  $thankYou="<p>Thank you! Your message has been sent.</p>";
+
+
+?>
